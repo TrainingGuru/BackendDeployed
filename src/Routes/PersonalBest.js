45fe0ce56750eddc1d11547634b9afa562671a74
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-02-03 19:13:49 +0000

    ready for push and merger

--- src/Routes/PersonalBest.js
+++ src/Routes/PersonalBest.js
@@ -4,7 +4,7 @@ const pbController = require('../Controllers/PersonalBestController')
 
 const router = express.Router();
 
-
+router.get("/Client/:id",pbController.GetLast3PBForClient);
 
 
 module.exports = router;
