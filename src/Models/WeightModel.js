4df2db4951e528d4fefc4c4cde7667a7fa086dea
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-02-21 20:39:46 +0000

    CatchUpModel.js added

---
+++ src/Models/WeightModel.js
@@ -0,0 +1,39 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../Config/DatabaseConfig');
+const Clients = require("./ClientModel");
+const Exercise = require("./ExerciseModel");
+
+const Weight = sequelize.define("Weight",{
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    ClientID:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+            model: 'Client',
+            key: 'ClientID'
+        }
+    },
+    Date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    Weight:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+},{
+    tableName: 'ClientsWeight',
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false
+});
+
+Weight.hasOne(Clients,{foreignKey: 'ClientID'});
+Clients.hasMany(Weight,{foreignKey: 'ClientID'});
+
+module.exports = Weight;
