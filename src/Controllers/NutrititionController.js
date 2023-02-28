1cb775e174f5e686aa437c01781a304c4efb1274
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-02-28 15:05:10 +0000

    finding user from db for updating Total Fats

--- src/Controllers/NutrititionController.js
+++ src/Controllers/NutrititionController.js
@@ -6,7 +6,18 @@ const updateCaloriesTotalTarget = async (req,res) =>{
 }
 
 const updateFatsTotalTarget = async (req,res) =>{
+    let clientFromDB = await Client.findOne({
+        where : {
+            ClientID : req.params.id
+        }});
 
+    if(clientFromDB != null)
+    {
+
+    }
+    else{
+        return res.status(404).json("No User Found")
+    }
 }
 
 const updateCarbsTotalTarget = async (req,res) =>{
