c2da186f2516cd0d46cc9b106aad8449b691d9f2
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-02-28 14:49:25 +0000

    Update method names created

--- src/Controllers/NutrititionController.js
+++ src/Controllers/NutrititionController.js
@@ -1,6 +1,18 @@
 const Nutrition = require("../Models/NutritionModel")
 const Client = require("../Models/ClientModel");
 
-const createNutrition = async (req,res) =>{
+const updateCaloriesTotalTarget = async (req,res) =>{
+
+}
+
+const updateFatsTotalTarget = async (req,res) =>{
+
+}
+
+const updateCarbsTotalTarget = async (req,res) =>{
+
+}
+
+const updateProteinTotalTarget = async (req,res) =>{
 
 }
