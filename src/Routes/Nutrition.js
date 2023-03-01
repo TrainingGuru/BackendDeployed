5571ca6353e975e4025b20243488b5fae2763b05
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-03-01 18:32:12 +0000

    endpoint url created ready for testing

--- src/Routes/Nutrition.js
+++ src/Routes/Nutrition.js
@@ -5,7 +5,7 @@ const nutritionController = require('../Controllers/NutrititionController')
 const router = express.Router();
 
 //client id - Updates the total Fat Total Target
-router.put("/:id/FatTotal")
+router.put("/:id/FatTotal",nutritionController.updateFatsTotalTarget);
 
 
 
