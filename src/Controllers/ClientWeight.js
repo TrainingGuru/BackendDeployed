f4cac5097c650a45aea174479146d3ed440de080
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-03-14 12:50:59 +0000

    addClientWeight method coded

--- src/Controllers/ClientWeight.js
+++ src/Controllers/ClientWeight.js
@@ -1,4 +1,6 @@
 const ClientWeight = require("../Models/WeightModel");
+const Client = require("../Models/ClientModel");
+const Goals = require("../Models/GoalsModel");
 
 const addClientWeight = async (req, res) => {
 
@@ -7,6 +9,19 @@ const addClientWeight = async (req, res) => {
         Date : req.body.Date,
         Weight : req.body.Weight
     }
+    let client = await Client.findOne({where : {
+            ClientID : req.params.clientId,
+        }});
 
-
+    if(client == null){
+        return res.status(404).json(`Client ${req.params.clientId} Not found`)
+    }else{
+        if(clintWeight.Weight === "" || clintWeight.Weight == null || clintWeight.Date === "" || clintWeight.Date == null){
+            return res.status(400).json("Missing Information in the body")
+        } else{
+            ClientWeight.create(clintWeight).then((clintWeightToAdd) => res.status(201).json(clintWeightToAdd))
+                .catch((err) => { res.status(400).send(err);
+                });
+        }
+    }
 }
