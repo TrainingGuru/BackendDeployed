0c4af8c0c7d5448c7e825183e435ec2a7cdfbaa5
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-03-19 19:57:04 +0000

    scheduleCatchUp url created

--- src/Routes/CatchUp.js
+++ src/Routes/CatchUp.js
@@ -4,5 +4,7 @@ const catchUpController = require('../Controllers/CatchUpController')
 
 const router = express.Router();
 
+router.post("/:clientId",catchUpController.scheduleCatchUp)
+
 
 module.exports = router;
