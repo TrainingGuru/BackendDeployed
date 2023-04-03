b35fb8c6d39cdde4903a29654742953524a72730
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-04-03 14:30:55 +0100

    start of the function created to test

--- src/Utilities/Utils.js
+++ src/Utilities/Utils.js
@@ -1,9 +1,20 @@
+const Exercise = require("../Models/ExerciseModel");
+const { Op } = require("sequelize");
 //Helpers
 
 const AddExercises = async (TrainerWorkoutid,Exercises) => {
 
     for(let i = 0; i< Exercises.length; i++)
     {
+        let DbExercise = await Exercise.findOne({
+            where : {
+                Name : {
+                    [Op.like] : Exercises[i]
+                }
+            }
+        })
+
+        console.log(DbExercise);
 
     }
 }
