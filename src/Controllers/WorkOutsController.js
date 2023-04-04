d69bdc5e5fb933d951a57a5df33dd59e8ec8ef06
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-04-04 16:30:51 +0100

    TotalWeightLifted coded

--- src/Controllers/WorkOutsController.js
+++ src/Controllers/WorkOutsController.js
@@ -193,9 +193,22 @@ const CreateAWorkout = async (req,res) => {
     }else{
         return res.status(400).json("Error Couldnt Create Workout")
     }
+}
 
+const TotalWeightLifted = async (req,res) => {
 
+    let TotalWeight = await ClientWorkOut.findAll({
+        where : {
+            Week : req.params.wk
+        },
+        attributes : [[Sequelize.fn('sum',Sequelize.col('TotalWeightLifted')),'Total']]
+    })
 
+    if(TotalWeight.length <=0 || TotalWeight == null){
+        return res.status(404).json("No Weight Found")
+    }else{
+        return res.status(200).json(TotalWeight)
+    }
 }
 
 
